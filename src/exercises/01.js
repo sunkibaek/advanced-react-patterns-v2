8d338e2b63eb74006bde5b7196f26226c9c1ca2f
Sunki Baek <sunki.baek@gmail.com>
2020-04-18 21:10:18 -0700

    Refactor with functional component

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -1,43 +1,18 @@
 // Building the toggle component
-
-import React from 'react'
-// 🐨 uncomment this import to get the switch component.
-// It takes an `onClick` and an `on` prop
+import React, {useState} from 'react'
 import {Switch} from '../switch'
 
-class Toggle extends React.Component {
-  // 🐨 this toggle component is going to need to have state for `on`
+const Toggle = ({onToggle}) => {
+  const [on, setOn] = useState(false)
 
-  constructor(props) {
-    super(props)
+  const handleToggle = () => {
+    const newValue = !on
 
-    this.state = {on: false}
+    setOn(newValue)
+    onToggle(newValue)
   }
 
-  handleToggle = () => {
-    this.setState(
-      (prev) => ({
-        on: !prev.on,
-      }),
-      () => {
-        this.props.onToggle(this.state.on)
-      },
-    )
-  }
-
-  // You'll also want a method to handle when the switch is clicked
-  // which will update the `on` state and call the `onToggle` prop
-  // with the new `on` state.
-  //
-  // 💰 this.setState(newState, callback)
-  //
-  // The `callback` should be where you call `this.props.onToggle(this.state.on)`
-  //
-  // 💯 Use a state updater function for `newState` to avoid issues with batching
-  render() {
-    // 🐨 here you'll want to return the switch with the `on` and `onClick` props
-    return <Switch on={this.state.on} onClick={this.handleToggle} />
-  }
+  return <Switch on={on} onClick={handleToggle} />
 }
 
 // Don't make changes to the Usage component. It's here to show you how your
